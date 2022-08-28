e4d67af39a91fe93320e4d7be6e9db492b476305
Teshome Kurabachew <dear.teshe@gmail.com>
2022-08-28 21:15:35 +0300

    Setup project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import './App.scss';
+
+const App = () => (
+  <Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  </Router>
+);
+
+export default App;
