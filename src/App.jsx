d4b2b28c77216ed83b88ed6e680d76bb9f8e99fd
Teshome Kurabachew <dear.teshe@gmail.com>
2022-08-29 22:15:53 +0300

    Create data.json file

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
+import data from './assets/data.json';
 import './App.scss';
 
-const App = () => (
-  <Router>
-    <Routes>
-      <Route path="/" element={<Home />} />
-    </Routes>
-  </Router>
-);
+const App = () => {
+  const parsedData = JSON.parse(JSON.stringify(data));
+  console.log(parsedData);
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
+  );
+};
 
 export default App;
