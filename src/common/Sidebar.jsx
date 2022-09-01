8612467ac2fd941eebf0c06145df1b089318ffb6
Teshome Kurabachew <dear.teshe@gmail.com>
2022-09-01 23:20:24 +0300

    Add profile name

--- src/common/Sidebar.jsx
+++ src/common/Sidebar.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { FaExpand } from 'react-icons/fa';
 import styles from './Sidebar.module.scss';
 import avatar from '../assets/images/Professional Photo (2).jpg';
 
 const Sidebar = () => {
+  const user = useSelector((state) => state.main.user);
+  const { name, title, subtitle } = user;
+
   const {
     sidebar, sidebarHeader, sidebarMain, sidebarFooter, Frame, sidebarAvatar, sidebarAvatarCurtain,
-    imgIcon, lampLight, lampLightAvailable,
+    imgIcon, lampLight, lampLightAvailable, profile, profileName, profileSmText,
   } = styles;
   return (
     <div className={sidebar}>
@@ -21,6 +25,14 @@ const Sidebar = () => {
             <div className={lampLight}>
               <div className={lampLightAvailable} />
             </div>
+          </div>
+          <h3 className={profile}>
+            <Link to="/" className={profileName}>{name}</Link>
+          </h3>
+          <div className={profileSmText}>
+            {title}
+            <br />
+            {subtitle}
           </div>
         </div>
         <div className={sidebarMain}>
