cb8009dccf859b230f0a4cb56f03cd2ec8a9f785
Teshome Kurabachew <dear.teshe@gmail.com>
2022-09-02 10:10:21 +0300

    Add skill progress bar

--- src/common/Sidebar.jsx
+++ src/common/Sidebar.jsx
@@ -8,18 +8,21 @@ import avatar from '../assets/images/Professional Photo (2).jpg';
 const Sidebar = () => {
   const user = useSelector((state) => state.main.user);
   const skills = useSelector((state) => state.main.softSkills);
+  const hardSkills = useSelector((state) => state.main.skills);
 
   const {
     name, title, subtitle, status, phone, address,
   } = user;
 
-  const stroke = (value) => 292.273 - (292.273 * value) / 100;
+  const circleStroke = (value) => 292.273 - (292.273 * value) / 100;
+  const barStroke = (value) => 100 - (100 * value) / 100;
 
   const {
     sidebar, sidebarHeader, sidebarMain, sidebarFooter, Frame, sidebarAvatar, sidebarAvatarCurtain,
     imgIcon, lampLight, lampLightAvailable, profile, profileName, profileSmText, sidebarMainContent,
     sidebarMainTable, sidebarMainDivider, sidebarMainSoftskills, sidebarMainSoftskillsItems,
-    circleProgress, circleProgressText,
+    circleProgress, circleProgressText, sidebarMainHardskills, sidebarMainHardskillsItems,
+    sidebarMainHardskillsItemsHeading, sidebarMainHardskillsItemsProgress, progressbarText,
   } = styles;
   return (
     <div className={sidebar}>
@@ -68,7 +71,7 @@ const Sidebar = () => {
                   <div className={circleProgress}>
                     <svg viewBox="0 0 100 100" style={{ display: 'block', width: '100%' }}>
                       <path d="M 50,50 m 0,-46.5 a 46.5,46.5 0 1 1 0,93 a 46.5,46.5 0 1 1 0,-93" stroke="#eee" strokeWidth="7" fillOpacity="0" />
-                      <path d="M 50,50 m 0,-46.5 a 46.5,46.5 0 1 1 0,93 a 46.5,46.5 0 1 1 0,-93" stroke="#555" strokeWidth="7" fillOpacity="0" style={{ strokeDasharray: '292.273, 292.273', strokeDashoffset: stroke(skill.level) }} />
+                      <path d="M 50,50 m 0,-46.5 a 46.5,46.5 0 1 1 0,93 a 46.5,46.5 0 1 1 0,-93" stroke="#555" strokeWidth="7" fillOpacity="0" style={{ strokeDasharray: '292.273, 292.273', strokeDashoffset: circleStroke(skill.level) }} />
                     </svg>
                     <div className={circleProgressText}>
                       {skill.level}
@@ -79,6 +82,32 @@ const Sidebar = () => {
               ))}
             </div>
             <div className={sidebarMainDivider} />
+            <div className={sidebarMainHardskills}>
+              {hardSkills.map((skill) => (
+                <div className={sidebarMainHardskillsItems} key={skill.id}>
+                  <div className={sidebarMainHardskillsItemsHeading}>
+                    <h4>{skill.name}</h4>
+                  </div>
+                  <div className={sidebarMainHardskillsItemsProgress}>
+                    <div style={{ position: 'relative' }}>
+                      <svg viewBox="0 0 100 1.72" preserveAspectRatio="none" style={{ width: '100%', height: '100%' }}>
+                        <path d="M 0,0.86 L 100,0.86" stroke="#eee" strokeWidth="1.72" fillOpacity="0" />
+                        <path
+                          d="M 0,0.86 L 100,0.86"
+                          stroke="#555"
+                          strokeWidth="1.72"
+                          fillOpacity="0"
+                          style={{ strokeDasharray: '100 100', strokeDashoffset: barStroke(skill.level) }}
+                        />
+                      </svg>
+                      <div className={progressbarText}>
+                        {`${skill.level}%`}
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </div>
           </div>
         </div>
         <div className={sidebarFooter}>
