8e610db9a85f03f8e74b513523a291bf161a9e30
Teshome Kurabachew <dear.teshe@gmail.com>
2022-09-29 20:04:20 +0300

    Remove sidebar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,32 +1,33 @@
 import React from 'react';
-import { GiHamburgerMenu } from 'react-icons/gi';
-import { MdClose } from 'react-icons/md';
-import Sidebar from '../common/Sidebar';
+// import { GiHamburgerMenu } from 'react-icons/gi';
+// import { MdClose } from 'react-icons/md';
+// import Sidebar from '../common/Sidebar';
 import Main from '../components/Main';
 import style from './Home.module.scss';
 
 const Home = () => {
-  const [sidebarClass, setSidebarClass] = React.useState('');
+  // const [sidebarClass, setSidebarClass] = React.useState('');
   const {
-    main, mainContainer, mainWrapper, hamburgerMenu,
+    main, mainContainer, mainWrapper,
   } = style;
 
-  const handleSidebar = () => {
-    if (sidebarClass === '') {
-      setSidebarClass('active');
-    } else {
-      setSidebarClass('');
-    }
-  };
+  // const handleSidebar = () => {
+  //   if (sidebarClass === '') {
+  //     setSidebarClass('active');
+  //   } else {
+  //     setSidebarClass('');
+  //   }
+  // };
 
-  const icon = sidebarClass === '' ? <GiHamburgerMenu className={hamburgerMenu} onClick={handleSidebar} /> : <MdClose className={hamburgerMenu} onClick={handleSidebar} />;
-
+  /* const icon = sidebarClass === '' ? <GiHamburgerMenu className={hamburgerMenu}
+  onClick={handleSidebar} /> : <MdClose className={hamburgerMenu} onClick={handleSidebar} />;
+*/
   return (
     <div className={main}>
       <div className={mainWrapper}>
         <div className={mainContainer}>
-          <Sidebar sidebarClass={sidebarClass} />
-          {icon}
+          {/* <Sidebar sidebarClass={sidebarClass} />
+          {icon} */}
           <Main />
         </div>
       </div>
